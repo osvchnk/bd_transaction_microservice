3c1aea044b1b3f98c8115ed0e80ef270ff0e8953
osvchnk <osvchnk@gmail.com>
2023-05-04 02:56:20 +0300

    + schema converter

---
+++ app/services/schema_converter.py
@@ -0,0 +1,26 @@
+from app.models.transaction import TrType, Transaction
+from app.schemas.transaction import TransactionInOutSchema, TransactionOutSchema, TransactionReportOutSchema
+
+
+class TransactionSchemaConverter:
+    @staticmethod
+    def convert_schema(transaction: Transaction) -> \
+            TransactionInOutSchema | TransactionOutSchema | TransactionReportOutSchema:
+
+        tr_type = transaction.type
+        general_transaction_schema = TransactionOutSchema.from_orm(transaction)
+
+        if tr_type == TrType.IN:
+            in_schema = TransactionInOutSchema.from_orm(transaction.transaction_in)
+            in_schema.transaction = general_transaction_schema
+            return in_schema
+
+        if tr_type == TrType.OUT:
+            out_schema = TransactionOutSchema.from_orm(transaction.transaction_out)
+            out_schema.transaction = general_transaction_schema
+            return out_schema
+
+        if tr_type == TrType.REPORT:
+            report_schema = TransactionReportOutSchema.from_orm(transaction.transaction_report)
+            report_schema.transaction = general_transaction_schema
+            return report_schema
