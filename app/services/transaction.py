08e637497823355f147297c7ffaf65444741b814
osvchnk <osvchnk@gmail.com>
2023-05-04 02:55:44 +0300

    add TransactionService

--- app/services/transaction.py
+++ app/services/transaction.py
@@ -1,12 +1,11 @@
 from fastapi import HTTPException
 
-from app.models.transaction import TrPaymentStatus, TrType, Transaction, TransactionIn, TransactionOut, \
-    TransactionReport
-from app.repositories.transaction import TransactionRepository
+from app.models.transaction import TrPaymentStatus, TrType, Transaction
+from app.repositories import *
 from app.schemas.payment import PaymentUpdate
-from app.schemas.transaction import TransactionInCreateSchema, TransactionOutCreateSchema, \
-    TransactionReportCreateSchema, TransactionOutOutSchema, TransactionAllInfoSchema, TransactionOutSchema, \
+from app.schemas.transaction import TransactionOutOutSchema, TransactionOutSchema, \
     TransactionInOutSchema, TransactionReportOutSchema
+from app.services.schema_converter import TransactionSchemaConverter
 
 
 class TransactionService:
@@ -15,87 +14,87 @@ class TransactionService:
         self.repository: TransactionRepository = TransactionRepository()
 
     @staticmethod
-    def get_schema(tr_type: TrType):
-        if tr_type == TrType.IN:
-            return TransactionInOutSchema
-        elif tr_type == TrType.OUT:
-            return TransactionOutOutSchema
-        elif tr_type == TrType.REPORT:
-            return TransactionReportOutSchema
-        else:
-            raise ValueError(f"Invalid transaction type: {tr_type}")
-
-    @staticmethod
     def hash_transaction(transaction: Transaction):
         pass
 
     @staticmethod
-    async def transaction_to_schema(transaction: Transaction):
-        tr_type = transaction.type
-        transaction_out_schema = TransactionOutSchema.from_orm(transaction)
-
+    def get_repo_and_schema(tr_type: TrType):
         if tr_type == TrType.IN:
-            return TransactionAllInfoSchema(
-                transaction=transaction_out_schema,
-                info=TransactionInOutSchema.from_orm(transaction.transaction_in))
-        if tr_type == TrType.OUT:
-            return TransactionAllInfoSchema(
-                transaction=transaction_out_schema,
-                info=TransactionOutOutSchema.from_orm(transaction.transaction_out))
-        if tr_type == TrType.REPORT:
-            return TransactionAllInfoSchema(
-                transaction=transaction_out_schema,
-                info=TransactionReportOutSchema.from_orm(transaction.transaction_report))
+            return TransactionInRepository, TransactionInOutSchema
+        elif tr_type == TrType.OUT:
+            return TransactionOutRepository, TransactionOutOutSchema
+        elif tr_type == TrType.REPORT:
+            return TransactionReportRepository, TransactionReportOutSchema
+        else:
+            raise ValueError(f"Invalid transaction type: {tr_type}")
 
-    async def get_transactions(self) -> list[TransactionAllInfoSchema]:
-        transactions = await self.repository.get_transactions()
-        response = []
-        for transaction in transactions:
-            response.append(await self.transaction_to_schema(transaction))
-        return response
-
-    async def create_transaction_in(self, transaction: TransactionInCreateSchema):
-        return await self.repository.create_transaction(TrType.IN, transaction)
-
-    async def create_transaction_out(self, transaction: TransactionOutCreateSchema):
-        return await self.repository.create_transaction(TrType.OUT, transaction)
-
-    async def create_transaction_report(self, transaction: TransactionReportCreateSchema):
-        return await self.repository.create_transaction(TrType.REPORT, transaction)
-
-    async def update_payment_status(self, transaction_id: int, payment_update: PaymentUpdate):
+    async def get_transaction_by_id(self, transaction_id: int) -> \
+            None | TransactionInOutSchema | TransactionOutSchema | TransactionReportOutSchema:
         transaction = await self.repository.get_transaction_by_id(transaction_id)
         if transaction is None:
+            return None
+        return TransactionSchemaConverter.convert_schema(transaction)
+
+    async def get_transaction_by_user_hash(self, user_hash: str) -> \
+            list[TransactionInOutSchema | TransactionOutSchema | TransactionReportOutSchema] | None:
+        transactions = await self.repository.get_transactions_by_user_hash(hash=user_hash)
+        result = []
+        for transaction in transactions:
+            result.append(TransactionSchemaConverter.convert_schema(transaction))
+        return result
+
+    async def get_transactions_by_block_id(self, block_id: int) -> \
+            list[TransactionInOutSchema | TransactionOutSchema | TransactionReportOutSchema] | None:
+
+        transactions = await self.repository.get_transactions_by_block_id(block_id)
+        result = []
+        for transaction in transactions:
+            result.append(TransactionSchemaConverter.convert_schema(transaction))
+        return result
+
+    async def update_payment_status(self, transaction_id: int, payment_update: PaymentUpdate):
+        general_transaction = await self.repository.get_transaction_by_id(transaction_id)
+
+        if general_transaction is None:
             raise HTTPException(status_code=404, detail=f"Transaction not found")
-        if transaction.type is TrType.REPORT:
+
+        if general_transaction.type is TrType.REPORT:
             raise HTTPException(status_code=405, detail=f"Method not allowed for the transaction type")
-        schema = self.get_schema(tr_type=transaction.type)
-        await self.repository.update_payment_info(transaction,
-                                                  payment_status=payment_update.status,
-                                                  payment_id=payment_update.payment_id)
+
+        repo, schema = self.get_repo_and_schema(tr_type=general_transaction.type)
+        repo_obj = repo()
+        # create repository instance so that requests to get and update will have one db session.
+        # It is necessary for working with one object from db.
+
+        transaction = await repo_obj.get_transaction_by_tr_id(tr_id=transaction_id)
+
+        if transaction.payment_status == TrPaymentStatus.SUCCESS:
+            # raise exception if payment status is already success
+            raise HTTPException(status_code=405, detail=f"Payment status already success")
+
+        transaction.payment_status = payment_update.status
+        transaction.payment_id = payment_update.payment_id
+        result = await repo_obj.update(transaction)
 
         # if PaymentUpdate.status == TrPaymentStatus.SUCCESS:
         #     self.hash_transaction()
 
-        full_transaction = await self.repository.get_full_transaction_by_id(transaction_id, transaction.type)
-        return TransactionAllInfoSchema(transaction=TransactionOutSchema.from_orm(transaction),
-                                        info=schema.from_orm(full_transaction))
-
-    async def get_transaction_by_id(self, transaction_id) -> TransactionAllInfoSchema | None:
-        transaction = await self.repository.get_transaction_by_id(transaction_id)
-        if transaction is None:
-            raise HTTPException(status_code=404, detail=f"Transaction not found")
-
-        tr_type = transaction.type
-        transaction_out_schema = TransactionOutSchema.from_orm(transaction)
-        schema = self.get_schema(tr_type=tr_type)
-        full_transaction = await self.repository.get_full_transaction_by_id(transaction_id, tr_type)
-        return TransactionAllInfoSchema(transaction=transaction_out_schema, info=schema.from_orm(full_transaction))
+        return schema.from_orm(result)
 
     async def sign_transaction(self, transaction_id: int, user_hash: str, sign: str):
-        transaction = await self.repository.get_transaction_in_by_id(transaction_id)
-        if transaction.tourist_hash != user_hash:
+        general_transaction = await self.repository.get_transaction_by_id(transaction_id)
+
+        if general_transaction.user_hash != user_hash:
             raise HTTPException(status_code=405, detail=f"Method not allowed. User hash doesn't match")
-        if transaction.payment_status != TrPaymentStatus.SUCCESS:
-            raise HTTPException(status_code=405, detail=f"Method not allowed. Not paid")
-        return await self.repository.add_sign(transaction, sign)
+
+        repo, schema = self.get_repo_and_schema(general_transaction.type)
+        transaction = await repo().get_transaction_by_tr_id(tr_id=transaction_id)
+
+        if general_transaction.type != TrType.REPORT:
+            if transaction.payment_status != TrPaymentStatus.SUCCESS:
+                raise HTTPException(status_code=405, detail=f"Method not allowed. Not paid")
+
+        general_transaction.tr_sign = sign
+        result = await self.repository.update(general_transaction)
+
+        return TransactionSchemaConverter.convert_schema(result)
