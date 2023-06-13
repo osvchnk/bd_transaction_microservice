e8cb112421eb0fb17eb25863dc02e635d5df8d32
osvchnk <osvchnk@gmail.com>
2023-06-13 07:29:54 +0300

    + updates

--- app/config.py
+++ app/config.py
@@ -7,3 +7,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"))
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+RABBIT_HOST = os.getenv("RABBIT_HOST")
+RABBIT_PORT = os.getenv("RABBIT_PORT")
+RABBIT_EXCHANGE = os.getenv("RABBIT_EXCHANGE")
+RABBIT_QUEUE = os.getenv("RABBIT_QUEUE")
+RABBIT_ROUTING_KEY = os.getenv("RABBIT_ROUTING_KEY")
--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from app.routers import transaction
-
+from app.routers import transaction, transaction_in, transaction_out, transaction_report
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8000",
+    "http://localhost:8004",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(transaction.router)
+app.include_router(transaction_in.router)
+app.include_router(transaction_out.router)
+app.include_router(transaction_report.router)
